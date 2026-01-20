49aa6f4817632f6c063fe357bca60db0d9c1ba5b
MinaAl5al <minanasser82018@gmail.com>
2026-01-20 22:21:39 +0200

    Topic Exchange

--- DirctExchange.Consumer/Program.cs
+++ DirctExchange.Consumer/Program.cs
@@ -8,74 +8,99 @@ namespace DirctExchange.Consumer
     {
         public static async Task Main(string[] args)
         {
+            //var factory = new ConnectionFactory
+            //{
+            //    HostName = "host.docker.internal",
+            //    Port = 5672
+            //};
+
+            //await using var connection = await factory.CreateConnectionAsync();
+            //await using var channel = await connection.CreateChannelAsync();
+
+            //// اسم Queue ثابت
+            //var queueName = "qu-notification";
+
+            //// Declare Queue
+            //await channel.QueueDeclareAsync(
+            //    queue: queueName,q 
+            //    durable: true,
+            //    exclusive: false,
+            //    autoDelete: false
+            //);
+
+            //// =========================
+            //// Direct Exchange (amq.direct)
+            //// =========================
+            //var routingKeys = args.Length == 0
+            //    ? new[] { "key-notification" }
+            //    : args;
+
+            //foreach (var routingKey in routingKeys)
+            //{
+            //    await channel.QueueBindAsync(
+            //        queue: queueName,
+            //        exchange: "amq.direct",
+            //        routingKey: routingKey
+            //    );
+            //}
+
+            //// =========================
+            //// Fanout Exchange (amq.fanout)
+            //// =========================
+            //// Fanout ما بياخدش routing key
+            //await channel.QueueBindAsync(
+            //    queue: queueName,
+            //    exchange: "amq.fanout",
+            //    routingKey: string.Empty
+            //);
+
+            //Console.WriteLine(" [*] Waiting for messages (Direct + Fanout). To exit press CTRL+C");
+
+            //// Consumer
+            //var consumer = new AsyncEventingBasicConsumer(channel);
+
+            //consumer.ReceivedAsync += async (model, ea) =>
+            //{
+            //    var body = ea.Body.ToArray();
+            //    var message = Encoding.UTF8.GetString(body);
+
+            //    Console.WriteLine($" [x] Received '{ea.RoutingKey}':'{message}'");
+
+            //    await Task.CompletedTask;
+            //};
+
+            //await channel.BasicConsumeAsync(
+            //    queue: queueName,
+            //    autoAck: true,
+            //    consumer: consumer
+            //);
+
+            //await Task.Delay(Timeout.Infinite);
+
             var factory = new ConnectionFactory
             {
-                HostName = "host.docker.internal",
-                Port = 5672
+                HostName = "localhost"
             };
-
             await using var connection = await factory.CreateConnectionAsync();
             await using var channel = await connection.CreateChannelAsync();
-
-            // اسم Queue ثابت
-            var queueName = "qu-notification";
-
-            // Declare Queue
-            await channel.QueueDeclareAsync(
-                queue: queueName,
-                durable: true,
-                exclusive: false,
-                autoDelete: false
-            );
-
-            // =========================
-            // Direct Exchange (amq.direct)
-            // =========================
-            var routingKeys = args.Length == 0
-                ? new[] { "key-notification" }
-                : args;
-
-            foreach (var routingKey in routingKeys)
-            {
-                await channel.QueueBindAsync(
-                    queue: queueName,
-                    exchange: "amq.direct",
-                    routingKey: routingKey
-                );
-            }
-
-            // =========================
-            // Fanout Exchange (amq.fanout)
-            // =========================
-            // Fanout ما بياخدش routing key
-            await channel.QueueBindAsync(
-                queue: queueName,
-                exchange: "amq.fanout",
-                routingKey: string.Empty
-            );
-
-            Console.WriteLine(" [*] Waiting for messages (Direct + Fanout). To exit press CTRL+C");
-
-            // Consumer
+            Console.WriteLine("Connection established.");
             var consumer = new AsyncEventingBasicConsumer(channel);
-
             consumer.ReceivedAsync += async (model, ea) =>
             {
                 var body = ea.Body.ToArray();
                 var message = Encoding.UTF8.GetString(body);
-
                 Console.WriteLine($" [x] Received '{ea.RoutingKey}':'{message}'");
-
                 await Task.CompletedTask;
             };
-
-            await channel.BasicConsumeAsync(
-                queue: queueName,
+            channel.BasicConsumeAsync(
+                queue: "q.health3",
                 autoAck: true,
                 consumer: consumer
             );
+            Console.WriteLine("Press [Enter] to  Exit .");
+            Console.ReadLine();
 
-            await Task.Delay(Timeout.Infinite);
+
         }
     }
 }
--- DirctExchange.Producer/Program.cs
+++ DirctExchange.Producer/Program.cs
@@ -8,81 +8,100 @@ namespace DirctExchange.Producer
     {
         public static async Task Main(string[] args)
         {
+            //var factory = new ConnectionFactory
+            //{
+            //    HostName = "host.docker.internal",
+            //    Port = 5672
+            //};
+
+            //await using var connection = await factory.CreateConnectionAsync();
+            //await using var channel = await connection.CreateChannelAsync();
+
+            //// الرسالة
+            //var severity = args.Length > 0 ? args[0] : "key-notification";
+            //var message = args.Length > 1
+            //    ? string.Join(" ", args.Skip(1))
+            //    : "Hello World!";
+
+            //var body = Encoding.UTF8.GetBytes(message);
+
+            //// 1️⃣ إرسال Direct
+            //await channel.BasicPublishAsync(
+            //    exchange: "amq.direct",
+            //    routingKey: severity,
+            //    body: body
+            //);
+            //Console.WriteLine($" [x] Sent Direct '{severity}':'{message}'");
+
+            //// 2️⃣ إرسال Fanout
+            //await channel.BasicPublishAsync(
+            //    exchange: "amq.fanout",
+            //    routingKey: string.Empty,
+            //    body: body
+            //);
+            //Console.WriteLine($" [x] Sent to Fanout: '{message}'");
+
+            //// ========================
+            //// Example: Consumer for Fanout Queues
+            //// ========================
+            //string[] fanoutQueues = { "q.fanout1", "q.fanout2", "q.fanout3", "q.fanout4", "q.fanout5" };
+
+            //foreach (var queueName in fanoutQueues)
+            //{
+            //    // Declare each queue
+            //    await channel.QueueDeclareAsync(
+            //        queue: queueName,
+            //        durable: true,
+            //        exclusive: false,
+            //        autoDelete: false
+            //    );
+
+            //    // Bind to fanout exchange
+            //    await channel.QueueBindAsync(
+            //        queue: queueName,
+            //        exchange: "amq.fanout",
+            //        routingKey: string.Empty
+            //    );
+
+            //    // Consumer
+            //    var consumer = new AsyncEventingBasicConsumer(channel);
+            //    consumer.ReceivedAsync += async (model, ea) =>
+            //    {
+            //        var bodyReceived = ea.Body.ToArray();
+            //        var msgReceived = Encoding.UTF8.GetString(bodyReceived);
+            //        Console.WriteLine($" [x] {queueName} Received: '{msgReceived}'");
+            //        await Task.CompletedTask;
+            //    };
+
+            //    await channel.BasicConsumeAsync(
+            //        queue: queueName,
+            //        autoAck: true,
+            //        consumer: consumer
+            //    );
+            //}
+
+            //// Keep running
+            //Console.WriteLine("Press CTRL+C to exit.");
+            //await Task.Delay(Timeout.Infinite);
+
+
             var factory = new ConnectionFactory
             {
-                HostName = "host.docker.internal",
-                Port = 5672
+                HostName = "localhost"
             };
-
             await using var connection = await factory.CreateConnectionAsync();
             await using var channel = await connection.CreateChannelAsync();
-
-            // الرسالة
-            var severity = args.Length > 0 ? args[0] : "key-notification";
-            var message = args.Length > 1
-                ? string.Join(" ", args.Skip(1))
-                : "Hello World!";
-
+            Console.WriteLine("Connection established.");
+            const string message = "Test Topic Exchange ";
             var body = Encoding.UTF8.GetBytes(message);
-
-            // 1️⃣ إرسال Direct
             await channel.BasicPublishAsync(
-                exchange: "amq.direct",
-                routingKey: severity,
+                exchange: "amq.topic",
+                routingKey: "h.y",
                 body: body
             );
-            Console.WriteLine($" [x] Sent Direct '{severity}':'{message}'");
-
-            // 2️⃣ إرسال Fanout
-            await channel.BasicPublishAsync(
-                exchange: "amq.fanout",
-                routingKey: string.Empty,
-                body: body
-            );
-            Console.WriteLine($" [x] Sent to Fanout: '{message}'");
-
-            // ========================
-            // Example: Consumer for Fanout Queues
-            // ========================
-            string[] fanoutQueues = { "q.fanout1", "q.fanout2", "q.fanout3", "q.fanout4", "q.fanout5" };
-
-            foreach (var queueName in fanoutQueues)
-            {
-                // Declare each queue
-                await channel.QueueDeclareAsync(
-                    queue: queueName,
-                    durable: true,
-                    exclusive: false,
-                    autoDelete: false
-                );
-
-                // Bind to fanout exchange
-                await channel.QueueBindAsync(
-                    queue: queueName,
-                    exchange: "amq.fanout",
-                    routingKey: string.Empty
-                );
-
-                // Consumer
-                var consumer = new AsyncEventingBasicConsumer(channel);
-                consumer.ReceivedAsync += async (model, ea) =>
-                {
-                    var bodyReceived = ea.Body.ToArray();
-                    var msgReceived = Encoding.UTF8.GetString(bodyReceived);
-                    Console.WriteLine($" [x] {queueName} Received: '{msgReceived}'");
-                    await Task.CompletedTask;
-                };
-
-                await channel.BasicConsumeAsync(
-                    queue: queueName,
-                    autoAck: true,
-                    consumer: consumer
-                );
-            }
-
-            // Keep running
-            Console.WriteLine("Press CTRL+C to exit.");
-            await Task.Delay(Timeout.Infinite);
+            Console.WriteLine($" [x] Sent Topic '{message}'");
+            Console.WriteLine($"Press [Enter ] to Exit .");
+            Console.ReadLine();
         }
     }
 }
